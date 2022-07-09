d420060d932dff5bd7e4f4a574a04d853834501e
Cancervior <pradeepthichowdary99@gmail.com>
2022-07-09 19:53:58 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+    <div className='container'>
+
+
+          <div className='mainContent'>
+            <h3>
+
+MY CANCER JOURNEY
+
+</h3>
+<span> After 42 cannulas, 31 blood samples, 15 scans, 2 surgeries, and a biopsy I’m finally glad to say that I’m “Cancer-free”. It is the best news our family has received after almost half a year of hardship. </span>
+<h6></h6>
+<p>Just when I was about to start my career as a doctor in December 2021, I was diagnosed with Non – Hodgkin’s lymphoma. I had no family history. I do not smoke or drink. I did not have any chronic illness in the past. I exercise and follow a healthy lifestyle. My cancer diagnosis was a huge shock to me as everything was going according to plan.
+<p> It all began with a sore throat which lasted for more than 2 weeks in November 2021. Upon examination, my GP diagnosed it as tonsillitis and prescribed me some antibiotics. Despite using multiple courses of antibiotics it did not get any better and gradually the swelling and redness became one-sided.  
+  <p>During this time, I was having horrible pain in my throat (on the left side), difficulty in swallowing, fatigue and I lost some weight. So, I went back to my GP two weeks later. He referred me to the ENT department after examining me where I was admitted for a week and treated for a peritonsillar abscess (pus collection around tonsils). 
+
+  </p>My doctor then ordered an MRI scan as there was no improvement in my situation. It was pretty terrifying to lie down in a partially closed tube without moving my head or breathing heavily for almost an hour with a small break in between. I was sent home the next day with some steroids and painkillers.
+</p>After a couple of days, I received a call from my doctor saying that the results of the scan were out and he wants to have a chat with me. I rushed to the hospital but I barely had any clue about cancer that was waiting to hit me in the face. The doctor had a really hard time uttering the c-word to me. He told me that the reports are suggesting a malignancy and it can be a lymphoma. 
+<span style={{color:"orange"}}></span>
+<p> He then explained everything in a very detailed and positive way, and he was shocked to see how I received the diagnosis. I was numb the whole time, and I did not feel a thing. I held it all together until I left the hospital and met my friend who was waiting for me and then I finally broke down.
+  
+</p>A week later I flew back to my country as I desperately needed the support of my family during the treatment process. We consulted an Ent specialist who decided to perform a tonsillectomy (removal of the affected tonsil) and the next thing I remember is lying down on an operation table two days later. The surgery did not go as planned and my tonsil was removed partially as I was bleeding excessively during the surgery. 
+<p>A sample from the surgery was sent for biopsy which revealed a High-grade Non-Hodgkin’s lymphoma (Stage 1E, DLBCL). The surgery caused severe pain in my throat and I couldn’t talk or eat well for a few days after the surgery. I was then referred to a Haematologist who decided that I will need at least six cycles of chemotherapy (4 cycles of R-CHOP + 2 cycles of high dose methotrexate) each 21 days apart. 
+
+</p>Just when I thought things couldn’t get any worse, I was advised to plan for my fertility preservation before starting with the chemo as there will be a chance of infertility in the future. For me, it was like watching a thriller movie with neither a clue of what was ahead of me nor a break to process anything. 
+<p> Preserving a woman’s fertility isn’t easy, one should receive 10-12 days of hormones (to your belly which can be very painful) and get scans every alternative day. On the 14th day of treatment, the doctor performs surgery to retrieve the eggs vaginally. It is usually done under general anaesthesia.</p>
+ <p> On the next day of my retrieval, I went back to my oncologist. My body was so exhausted from two surgeries in 3 weeks but I did not have the luxury of delaying my chemotherapy. I was admitted for two days in the hospital and the first dose of chemotherapy was given. Surprisingly, the chemo itself wasn’t as bad as I thought it would be.</p>
+ <p> However, my happiness was short-lived as I experienced severe side effects within a few hours of my discharge. Multiple episodes of loose stools, vomitings, nausea, sore mouth(mucositis), severe weakness, loss of appetite, and muscle spasms around the mouth which caused difficulty in eating and speaking.  It took almost two weeks for the side effects to subside. </p>
+ <p> With each session, I experienced new side effects. The worst of them all is losing my hair in bunches. Being a girl who loves long hair, it was traumatising to see strands of hair falling all over the place. I cried every single day for a week while my mom was combing my hair before I finally decided to chop it off and then go bald completely. Although I felt ugly for the first time in my life and hardly looked in the mirror for days, I must say I was at peace when I asked my sister to finally shave it as I did not have the courage to do so myself. </p>
+ <p> Apart from the hair fall, I have had a horrible experience post intrathecal injection of methotrexate (it’s given into the spine). I had a throbbing headache after 24 hours (It started over the sides of my head, gradually progressed towards the back and finally passed down to my neck). It was very disturbing as I couldn’t hold my neck without support for almost a week and I was very sensitive to light and sound. This presentation is called “Meningism” which is a side effect of intrathecal methotrexate administration.</p>
+ <p> Moreover, side effects such as Pins and needles, muscle pains, bone pain, bloating, constipation, acidity, nausea, severe weakness, anal tears, pain from iv fluids and cannulas, risk of infection due to low cell count, irregular/no periods were common with each session.</p>
+ <p> By the end of my treatment, I was scared of needles, scans, pills, surgeries and everything about hospitals. I’d be lying if I say I was strong throughout this journey because enduring week after week of treatments and facing complication upon complication was overwhelming both physically and emotionally.</p>
+ <p> This is the experience I wish I didn’t have but there was no escape as my only option was to endure and survive. I’m happy to finally say that all the sleepless nights, ocean full of tears, hardened veins, bald head, and weight gained finally paid off. Most people expect a cancer survivor to be okay since the treatment is done but it’s never completely over.</p>
+ <p> Cancer journey can be tricky. </p>
+   <p>There are periods of bliss and hopefulness followed by episodes of dark hopelessness. </p>
+<p>Therefore, it is important to </p>
+<p> •	Listen to your body.</p>
+<p>•	Take it one step at a time. </p>
+
+<p> •	Take time and focus on healing yourself physically and mentallY.</p>
+
+<p>•	Don’t force yourself into something until it's time.</p>
+
+<p>•	Don’t hesitate to seek help!</p>
+<p>Cancer has taken a part of me along with it. I’m not my old self anymore and I feel less confident but I’m grateful to be alive and breathing. This journey made me stronger and taught me many useful things on a professional and personal level.</p>
+<p>“Life is unpredictable”, so do not take anything for granted and make sure you hold your loved ones close. This would have been impossible without the selfless love of my family who took care of me like a small baby and continuously assured me that I can make it and it was going to be okay. I will forever be thankful to my family and friends for supporting me. 
+</p>
+<p>I was very confused about the idea of writing a blog as I wasn’t so sure about sharing the good, beautiful, bad and ugly parts of this journey with strangers. I have come to terms with this idea after realising how much blogs of cancer patients and connecting with cancer survivors on social media platforms helped me during my cancer process. The thing that motivated me the most to write a blog is sharing the things I have learnt during the process (not as a doctor but as a patient). </p>
+<p>It’s my sincere hope that reading about my journey would at least make one person suffering from cancer feel better and assure them that they are not alone.
+ Nothing about this journey is easy but remember that others are facing other worse types of cancers in later stages and dealing with even more horrific odds. 
+</p>
+<p>Although I feel completely lost at the moment as the cycle of healing is never-ending, I consider myself lucky to be diagnosed in the early stages and cured completely.
+
+</p>
+</p>
+<p>
+
+</p>
+<p>
+
+</p>
+&nbsp;
+<div style={{height:'100px'}}></div>
+
+
+
+
+<h3> IT’S OKAY!!!!!</h3>
+&nbsp;
+<p>It’s okay to be not okay.</p>
+<p>It’s okay to cry.</p>
+<p>It’s okay to be sad.</p>
+<p>It’s ok to feel bad.</p>
+<p>It’s okay to feel ugly.</p>
+<p>It’s okay to lose your hair.</p>
+<p>It’s okay to have scars.</p>
+<p>It’s okay to gain some weight.</p>
+<p>It’s okay to feel low.</p>
+<p>It’s okay to feel negative.</p>
+ <p>It’s okay to be not strong.</p>    
+  <p>It’s okay to feel less confident</p>     
+  <p>It’s okay to be annoyed. </p>
+  <p>It’s okay to feel messed up.</p>
+   <p>It’s okay to take a break.</p>    
+    <p>It’s okay to let everyone know.</p>   
+       <p>It’s okay to keep your journey private.</p>
+       <p>It’s okay to be not understood by anyone.</p>
+       <p>It’s okay to not have everything sorted out.</p>
+       <p> Its okay if your future looks different than you planned</p>
+       &nbsp;
+       <p> Because watever you are facing is temporary and it’s not going to last forever.</p>
+       <p>Please remember that you are strong, beautiful and bold.</p>
+       </div>
+         
+         
+         
+         
+
+
+            {/* <div className='intro1'></div> */}
+            {/* <div className='intro2'></div> */}
+
+          
+      </div>
+      </div>
   );
 }
 
